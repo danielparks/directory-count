8ca0919ca97d8cd2c9e50936b85c411dd692994c
Daniel Parks <dp+git@oxidized.org>
2022-08-08 20:25:05 -0700

    Debugging: https://github.com/BurntSushi/walkdir/issues/163

--- src/main.rs
+++ src/main.rs
@@ -29,12 +29,19 @@ fn cli(params: Params) -> anyhow::Result<()> {
             let entry = entry?; // FIXME print error and continue?
             let depth = entry.depth();
 
+            print!("{:6} ", if entry.file_type().is_dir() { "d" } else if entry.file_type().is_symlink() { "s" } else { "f" });
+            stdout.write_all(&entry.path().to_raw_bytes())?;
+            println!("");
+
             if depth >= depth_count.len() {
                 // Depth increased.
                 depth_count.resize(depth+1, 0);
             } else if depth+1 < depth_count.len() {
                 // Depth decreased; we finished looking through sub directory.
-                assert!(entry.file_type().is_dir(), "Left directory but next entry was not directory itself");
+                //assert!(entry.file_type().is_dir(), "Left directory but next entry was not directory itself");
+                if !entry.file_type().is_dir() {
+                    eprintln!("Left directory but next entry was not directory itself:");
+                }
 
                 let sum: usize = depth_count.drain(depth+1..).sum();
 
