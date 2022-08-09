eed1049d75911cf6a9e55230ac20567d77fa06cd
Daniel Parks <dp+git@oxidized.org>
2022-08-08 20:43:59 -0700

    Simplify by removing walkdir.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 use clap::Parser;
 use os_str_bytes::OsStrBytes;
-use std::io::Write;
+use std::io::{stdout, Write};
+use std::path::Path;
 use std::path::PathBuf;
 use std::process::exit;
-use walkdir::WalkDir;
 
 #[derive(Debug, clap::Parser)]
 #[clap(version, about)]
@@ -21,44 +21,29 @@ fn main() {
 }
 
 fn cli(params: Params) -> anyhow::Result<()> {
-    let mut stdout = std::io::stdout();
-    let mut depth_count: Vec<usize> = Vec::new();
-
     for parent in params.directories {
-        for entry in WalkDir::new(parent).contents_first(true) {
-            let entry = entry?; // FIXME print error and continue?
-            let depth = entry.depth();
-
-            print!("{:6} ", if entry.file_type().is_dir() { "d" } else if entry.file_type().is_symlink() { "s" } else { "f" });
-            stdout.write_all(&entry.path().to_raw_bytes())?;
-            println!("");
-
-            if depth >= depth_count.len() {
-                // Depth increased.
-                depth_count.resize(depth+1, 0);
-            } else if depth+1 < depth_count.len() {
-                // Depth decreased; we finished looking through sub directory.
-                //assert!(entry.file_type().is_dir(), "Left directory but next entry was not directory itself");
-                if !entry.file_type().is_dir() {
-                    eprintln!("Left directory but next entry was not directory itself:");
-                }
-
-                let sum: usize = depth_count.drain(depth+1..).sum();
-
-                print!("{:6} ", sum);
-                stdout.write_all(&entry.path().to_raw_bytes())?;
-                println!("");
-
-                depth_count[depth] += sum;
-            } else if entry.file_type().is_dir() {
-                // Directory with no contents.
-                print!("{:6} ", 0);
-                stdout.write_all(&entry.path().to_raw_bytes())?;
-                println!("");
-            }
-
-            depth_count[depth] += 1;
-        }
+        walk(&parent)?;
     }
+
     Ok(())
+}
+
+fn walk(parent: &Path) -> anyhow::Result<usize> {
+    let mut count: usize = 0;
+
+    for entry in parent.read_dir()? {
+        let entry = entry?;
+
+        if entry.file_type()?.is_dir() {
+            count += walk(&entry.path())?
+        }
+
+        count += 1;
+    }
+
+    print!("{:6} ", count);
+    stdout().write_all(&parent.to_raw_bytes())?;
+    println!();
+
+    Ok(count)
 }
