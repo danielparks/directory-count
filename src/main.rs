b9c1fc98c2c8f5c86fe621d7946c818f4538ac80
Daniel Parks <dp+git@oxidized.org>
2023-07-01 20:52:50 -0700

    Use cargo-lints to centrally configure lints
    This updates the lint configuration to match my other projects. It
    includes a bunch more restriction lints.
    
    To run lints:
    
        cargo lints clippy --all-targets --all-features
    
    This requires [cargo-lints] to be installed.
    
    [cargo-lints]: https://crates.io/crates/cargo-lints

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
-#![forbid(unsafe_code)]
-#![warn(clippy::pedantic)]
-#![allow(clippy::let_underscore_untyped, clippy::map_unwrap_or)]
+//! `directory-count` executable.
 
-use anyhow::Result;
+// Most lint configuration is in lints.toml, but it doesn’t support forbid.
+#![forbid(unsafe_code)]
+
+use anyhow::{anyhow, Result};
 use clap::Parser;
 use os_str_bytes::OsStrBytes;
 use std::fmt::Display;
@@ -11,6 +12,7 @@ use std::path::Path;
 use std::path::PathBuf;
 use std::process::exit;
 
+/// Parameters for the executable.
 #[derive(Debug, clap::Parser)]
 #[clap(version, about)]
 struct Params {
@@ -25,6 +27,7 @@ fn main() {
     }
 }
 
+/// Call `walk()` on each of the parameters.
 fn cli(params: Params) -> Result<()> {
     for parent in params.directories {
         if let Err(error) = walk(&parent) {
@@ -35,30 +38,35 @@ fn cli(params: Params) -> Result<()> {
     Ok(())
 }
 
+/// Walk the directory tree under `parent` and print counts.
 fn walk(parent: &Path) -> Result<usize> {
     let mut count: usize = 1; // Count this directory.
 
     for entry in parent.read_dir()? {
-        count += match entry {
-            Ok(entry) => {
-                if entry.file_type()?.is_dir() {
-                    match walk(&entry.path()) {
-                        Ok(dir_count) => dir_count,
-                        Err(error) => {
-                            print_error(Some(&entry.path()), &error)?;
-                            1
+        count = count
+            .checked_add(match entry {
+                Ok(entry) => {
+                    if entry.file_type()?.is_dir() {
+                        match walk(&entry.path()) {
+                            Ok(dir_count) => dir_count,
+                            Err(error) => {
+                                print_error(Some(&entry.path()), &error)?;
+                                1
+                            }
                         }
+                    } else {
+                        1
                     }
-                } else {
+                }
+                Err(error) => {
+                    // FIXME should this at least show the parent path?
+                    print_error(None, &error)?;
                     1
                 }
-            }
-            Err(error) => {
-                // FIXME should this at least show the parent path?
-                print_error(None, &error)?;
-                1
-            }
-        }
+            })
+            .ok_or_else(|| {
+                anyhow!("cannot count to more than {}", usize::MAX)
+            })?;
     }
 
     print!("{count:6} ");
@@ -68,6 +76,7 @@ fn walk(parent: &Path) -> Result<usize> {
     Ok(count)
 }
 
+/// Report an error we encountered to the user.
 fn print_error<E>(path: Option<&Path>, error: &E) -> Result<()>
 where
     E: Display,
